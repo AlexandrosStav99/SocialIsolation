7517c79c06a5b7f3afcec9f408d416652750cf31
AlexandrosStav99 <102972703+AlexandrosStav99@users.noreply.github.com>
2026-09-20 21:58:51 +0300

    Use Node-resolvable imports in Payload config

--- payload.config.ts
+++ payload.config.ts
@@ -1,15 +1,15 @@
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
-import { getDatabaseUrl, getPayloadSecret } from "@/lib/config/server";
-import { ProviderOrganisations } from "@/payload/collections/ProviderOrganisations";
-import { Providers } from "@/payload/collections/Providers";
-import { Services } from "@/payload/collections/Services";
-import { ProviderUsers } from "@/payload/collections/ProviderUsers";
-import { ContactRequests } from "@/payload/collections/ContactRequests";
-import { ConsentRecords } from "@/payload/collections/ConsentRecords";
-import { EphemeralSessions } from "@/payload/collections/EphemeralSessions";
-import { AnonymousAnalyticsEvents } from "@/payload/collections/AnonymousAnalyticsEvents";
-import { ProviderAuditEvents } from "@/payload/collections/ProviderAuditEvents";
+import { getDatabaseUrl, getPayloadSecret } from "./lib/config/server.ts";
+import { ProviderOrganisations } from "./payload/collections/ProviderOrganisations.ts";
+import { Providers } from "./payload/collections/Providers.ts";
+import { Services } from "./payload/collections/Services.ts";
+import { ProviderUsers } from "./payload/collections/ProviderUsers.ts";
+import { ContactRequests } from "./payload/collections/ContactRequests.ts";
+import { ConsentRecords } from "./payload/collections/ConsentRecords.ts";
+import { EphemeralSessions } from "./payload/collections/EphemeralSessions.ts";
+import { AnonymousAnalyticsEvents } from "./payload/collections/AnonymousAnalyticsEvents.ts";
+import { ProviderAuditEvents } from "./payload/collections/ProviderAuditEvents.ts";
 
 export default buildConfig({
   secret: getPayloadSecret(),
