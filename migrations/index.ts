ae116ed13efc44878ca92ade641f46eeffd421a8
AlexandrosStav99 <102972703+AlexandrosStav99@users.noreply.github.com>
2026-09-26 18:13:40 +0300

    PROD-2: register authentication hardening migration

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260922_073920_initial_runtime_schema from './20260922_073920_initial_runtime_schema';
+import * as migration_20260926_152500_auth_hardening from './20260926_152500_auth_hardening';
 
 export const migrations = [
   {
     up: migration_20260922_073920_initial_runtime_schema.up,
     down: migration_20260922_073920_initial_runtime_schema.down,
     name: '20260922_073920_initial_runtime_schema'
+  },
+  {
+    up: migration_20260926_152500_auth_hardening.up,
+    down: migration_20260926_152500_auth_hardening.down,
+    name: '20260926_152500_auth_hardening'
   },
 ];
